43a53160f031f29bf6614402b0f5eee7daf2ace4
ms2sato <ms2.sato@gmail.com>
2020-09-01 11:13:37 +0900

    format

--- src/20200825_db_join/joins.sql
+++ src/20200825_db_join/joins.sql
@@ -1,32 +1,32 @@
 -- いろいろ試しましょう
 
 -- ユーザー一覧
-SELECT * from users;
+SELECT * FROM users;
 
 -- メンバー情報
-SELECT * from clubs join members on clubs.id = members.club_id;
+SELECT * FROM clubs JOIN members ON clubs.id = members.club_id;
 
 -- メンバーの総数
-SELECT count(*) from clubs join members on clubs.id = members.club_id;
+SELECT count(*) FROM clubs JOIN members ON clubs.id = members.club_id;
 
 -- とりあえず結合して全部出す
-SELECT * from users
-    join members on users.id = members.user_id
-    join clubs on clubs.id = members.club_id;
+SELECT * FROM users
+    JOIN members ON users.id = members.user_id
+    JOIN clubs ON clubs.id = members.club_id;
 
 -- ユーザーとクラブ名のリスト
-SELECT users.name, clubs.name from users
-    join members on users.id = members.user_id
-    join clubs on clubs.id = members.club_id;
+SELECT users.name, clubs.name FROM users
+    JOIN members ON users.id = members.user_id
+    JOIN clubs ON clubs.id = members.club_id;
 
 -- 部長さんだけ見る
-SELECT users.name, clubs.name from users
-    join members on users.id = members.user_id
-    join clubs on clubs.id = members.club_id
-    where members.role = 1;
+SELECT users.name, clubs.name FROM users
+    JOIN members ON users.id = members.user_id
+    JOIN clubs ON clubs.id = members.club_id
+    WHERE members.role = 1;
 
 -- ユーザーが幾つの部活に入っているか
-SELECT users.name, count(members.id) from users
-    join members on users.id = members.user_id
-    join clubs on clubs.id = members.club_id
-    group by users.id;
+SELECT users.name, count(members.id) FROM users
+    JOIN members ON users.id = members.user_id
+    JOIN clubs ON clubs.id = members.club_id
+    GROUP BY users.id;
