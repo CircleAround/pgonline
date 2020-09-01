4978c0c173ab74be358434cb17187283624a3850
ms2sato <ms2.sato@gmail.com>
2020-09-01 11:00:53 +0900

    typo

--- src/20200825_db_join/joins.sql
+++ src/20200825_db_join/joins.sql
@@ -12,21 +12,21 @@ SELECT count(*) from clubs join members on clubs.id = members.club_id;
 -- とりあえず結合して全部出す
 SELECT * from users
     join members on users.id = members.user_id
-    join clubs on clubs.id = members.club_id
+    join clubs on clubs.id = members.club_id;
 
 -- ユーザーとクラブ名のリスト
 SELECT users.name, clubs.name from users
     join members on users.id = members.user_id
-    join clubs on clubs.id = members.club_id
+    join clubs on clubs.id = members.club_id;
 
 -- 部長さんだけ見る
 SELECT users.name, clubs.name from users
     join members on users.id = members.user_id
     join clubs on clubs.id = members.club_id
-    where members.role = 1
+    where members.role = 1;
 
 -- ユーザーが幾つの部活に入っているか
 SELECT users.name, count(members.id) from users
     join members on users.id = members.user_id
     join clubs on clubs.id = members.club_id
-    group by users.id
+    group by users.id;
